76d6b8c2d8e0ef473e9a2f79a533ad744cd14dd4
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-07-20 22:08:21 +0530

    initial commit

---
+++ src/main/java/com/example/springdockerimageapp/SpringDockerImageAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockerimageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerImageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerImageAppApplication.class, args);
+	}
+
+}
